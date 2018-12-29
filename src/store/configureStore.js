aa378a3c91f588afa6518c2eba676a392dad5c0a
lei <oizhaolei@gmail.com>
2018-12-29 11:45:32 +0800

    pdfviewer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,15 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod');
-} else {
-  module.exports = require('./configureStore.dev');
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from '../reducers';
+import api from '../middleware/api';
+
+const middleware = [thunk, api];
+// Middleware you want to use in production:
+const enhancer = applyMiddleware(...middleware);
+
+export default function configureStore(initialState) {
+  // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
+  // See https://github.com/rackt/redux/releases/tag/v3.1.0
+  return createStore(rootReducer, initialState, enhancer);
 }
